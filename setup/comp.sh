93e71604828d760813d70e820a2a2e06d201d983
tomasantonelli <contact@tomas-antonelli.xyz>
2023-08-26 01:58:29 -0300

    Actualiza setup

--- setup/comp.sh
+++
@@ -1,3 +0,0 @@
-rm -f $1
-clear
-make $1 2>&1 | head -$2
--- setup/template.cpp
+++ setup/template.cpp
@@ -1,23 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-using i64 = int64_t;
 #define endl         '\n'
 #define forn(i,N)    for (int i = 0; i < int(N); i++)
+#define all(v)       begin(v), end(v)
 #define dbg(x)       cerr << #x << " = " << (x) << endl
 #define raya         cerr << "================" << endl
-#define all(v)       begin(v), end(v)
-#define pb           push_back
-#define mp           make_pair
-#define fst          first
-#define snd          second
 #define forall(it,v) for (auto it = begin(v); it != end(v); it++)
 #define printall(v)  forall(x,v) { cout << *x << " "; } cout << endl
-#define printpair(p) cout << "(" << p.fst << ", " << p.snd << ")" << endl
+#define printpair(p) cout << "(" << p.first << ", " << p.second << ")" << endl
 
 int main (void) {
-    ios::sync_with_stdio(0); cin.tie(0);
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-    
 
     return 0;
 }

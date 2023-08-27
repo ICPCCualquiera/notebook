ad8a4f4198b4d562a9ec7224049be882636c471e
tomasantonelli <contact@tomas-antonelli.xyz>
2023-08-27 17:27:42 -0300

    Actualiza implementación de Kahn

--- grafos/toposort_kahn.cpp
+++ grafos/toposort_kahn.cpp
@@ -1,34 +1,26 @@
 using AdjList = vector<vector<int>>;
 
-using Bag = priority_queue<int, vector<int>, greater<int>>;
-
-struct KahnToposort {
-    int N;
-    AdjList& G;
-    vector<int>& indegree;
-    vector<int> visit, res;
-    vector<int> Run () {
-        visit.resize(N);
-        Bag bag;
-        forn(u, N) if(indegree[u] == 0) bag.push(u);
-        while (bag.size()) {
-            int u = bag.top();
-            bag.pop();
-            res.push_back(u);
-            for (int v : G[u]) {
-                indegree[v]--;
-                if (indegree[v] == 0) bag.push(v);
-            }
+vector<int> KahnToposort (AdjList& G) {
+    int N = G.size();
+    vector<int> indegree(N), visit(N), res;
+    forn(u, N) for (int v : G[u]) indegree[v]++;
+    // Elegir crierio de priorizacion cambiando el orden en el que se sacan
+    // (por defecto el menor)
+    using Bag = priority_queue<int, vector<int>, greater<int>>;
+    Bag bag;
+    forn(u, N) if(indegree[u] == 0) bag.push(u);
+    while (bag.size()) {
+        int u = bag.top();
+        bag.pop();
+        res.push_back(u);
+        for (int v : G[u]) {
+            indegree[v]--;
+            if (indegree[v] == 0) bag.push(v);
         }
-        return res;
     }
-};
+    return res;
+}
 
 // Usar asi:
-int N = ...;
 AdjList G = {...};
-vector<int> indegree(N);
-forn(u, N) for (int v : G[u]) indegree[v]++;
-
-KahnToposort kahntoposort = {N, G, indegree};
-vector<int> topo_ord = kahntoposort.Run();
+vector<int> topo_ord = KahnToposort(G);

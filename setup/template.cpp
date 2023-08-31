fedfbf54ae27bd19c2d0b4a47e681972ec62cc8c
tomasantonelli <contact@tomas-antonelli.xyz>
2023-08-31 01:21:32 -0300

    Agrega cosas al template

--- setup/template.cpp
+++ setup/template.cpp
@@ -1,17 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define endl         '\n'
+#define forall(it,v) for (auto it = begin(v); it != end(v); it++)
 #define forn(i,N)    for (int i = 0; i < int(N); i++)
 #define all(v)       begin(v), end(v)
+#define mp(a,b)      make_pair(a,b)
+#define pb           push_back
+#define fst          first
+#define snd          second
+#define endl         '\n'
 #define dbg(x)       cerr << #x << " = " << (x) << endl
 #define raya         cerr << "================" << endl
-#define forall(it,v) for (auto it = begin(v); it != end(v); it++)
-#define printall(v)  forall(x,v) { cout << *x << " "; } cout << endl
-#define printpair(p) cout << "(" << p.first << ", " << p.second << ")" << endl
+template <typename A, typename B> ostream& operator << (ostream &o, pair<A,B>& p) { return o << p.fst << " " << p.snd; }
+template <typename T>             ostream& operator << (ostream& o, vector<T>& v) { forall(it,v) { o << *it << " "; } return o; }
 
 int main () { ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-
+    
 
     return 0;
 }

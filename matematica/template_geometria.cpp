674cc7f5094261263778566b7bb0444c9990d631
tomasantonelli <contact@tomas-antonelli.xyz>
2023-08-31 23:26:53 -0300

    Actualiza template geometria

--- matematica/template_geometria.cpp
+++ matematica/template_geometria.cpp
@@ -5,11 +5,11 @@ bool  flt_eq  (flt a, flt b) { return -EPS <= a - b && a - b <= EPS; }
 
 using Sca = long long;
 struct Vec { Sca x, y; };
-Vec  operator+ (Vec a, Vec b) { return { a.x + b.x, a.y + b.y }; }
-Vec  operator- (Vec a, Vec b) { return { a.x - b.x, a.y - b.y }; }
-Sca  operator* (Vec a, Vec b) { return a.x * b.x + a.y * b.y; }
-Sca  operator^ (Vec a, Vec b) { return a.x * b.y + a.y * b.x; }
-bool operator< (Vec a, Vec b) { return (a.x != b.x) ? (a.x < b.x) : (a.y < b.y); }
-ostream& operator<< (ostream &o, Vec& p) { auto x = make_pair(p.x, p.y); return o << x; }
+Vec  operator + (Vec a, Vec b) { return { a.x + b.x, a.y + b.y }; }
+Vec  operator - (Vec a, Vec b) { return { a.x - b.x, a.y - b.y }; }
+Sca  operator * (Vec a, Vec b) { return a.x * b.x + a.y * b.y; }
+Sca  operator ^ (Vec a, Vec b) { return a.x * b.y + a.y * b.x; }
+bool operator < (Vec a, Vec b) { return (a.x != b.x) ? (a.x < b.x) : (a.y < b.y); }
+ostream& operator << (ostream &o, Vec& p) { auto x = mp(p.x, p.y); return o << x; }
 
 Sca norma2 (Vec p) { return p.x * p.x + p.y * p.y; }

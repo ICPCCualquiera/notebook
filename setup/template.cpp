10a40183232a51ccb9b8255eeb747b4d38cebe85
tomasantonelli <contact@tomas-antonelli.xyz>
2023-09-03 16:08:29 -0300

    Agrega forr(i,a,b) a template

--- setup/template.cpp
+++ setup/template.cpp
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define forall(it,v) for (auto it = begin(v); it != end(v); it++)
-#define forn(i,N)    for (int i = 0; i < int(N); i++)
+#define forr(i,a,b)  for(int i = (a); i < (b); i++)
+#define forn(i,n)    forr(i,0,n)
 #define all(v)       begin(v), end(v)
 #define mp(a,b)      make_pair(a,b)
 #define pb           push_back
 #define fst          first
 #define snd          second
 #define endl         '\n'
-#define dbg(x)       cerr << #x << " = " << (x) << endl
+#define dprint(x)    cerr << #x << " = " << (x) << endl
 #define raya         cerr << "================" << endl
 #define templT       template <class T>
 #define templAB      template <class A, class B>
 templAB ostream& operator << (ostream& o, pair<A,B>& p) { return o << p.first << " " << p.second; }
 templT  ostream& operator << (ostream& o, vector<T>& v) { forall(it,v) { o << *it << " "; } return o; }
+
 
 int main () { ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 

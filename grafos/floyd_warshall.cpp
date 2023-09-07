b048af30810abf56dc6ac0c469e986ccbd5365a2
tomasantonelli <contact@tomas-antonelli.xyz>
2023-09-07 14:32:13 -0300

    Agrega Floyd-Warshal

---
+++ grafos/floyd_warshall.cpp
@@ -0,0 +1,13 @@
+using ll = long long;
+templT using Matriz = vector<vector<T>>;
+const ll INF = LLONG_MAX / 4;
+
+void FloydWarshall (Matriz<ll>& D) {
+   int N = D.size();
+   forn(u, N) D[u][u] = 0;
+   forn(k, N) forn(u, N) forn(v, N) if (D[u][k] < INF) if (D[k][v] < INF)
+      D[u][v] = min(D[u][v], D[u][k] + D[k][v]);
+   // Opcional: chequear ciclos negativos
+   forn(u, N) forn(v, N) forn(k, N) if (D[u][k] < INF && D[k][k] < 0 && D[k][v] < INF)
+      D[u][v] = -INF;
+}

ade00e8f0dd83945dbd1f182318eaf8c777d682d
tomasantonelli <contact@tomas-antonelli.xyz>
2023-09-15 15:24:36 -0300

    Fix error que deje en el template

--- setup/template_completo.cpp
+++ setup/template_completo.cpp
@@ -15,7 +15,7 @@ using namespace std;
 #define templAB      template <class A, class B>
 templAB ostream& operator << (ostream& o, pair<A,B>& p) { return o << p.fst << " " << p.snd; }
 templT  ostream& operator << (ostream& o, vector<T>& v) { forall(it,v) { o << *it << " "; } return o; }
-
+using ll = long long;
 
 int main (int argc, char** argv) { ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
    if (argc == 2) freopen("input", "r", stdin);

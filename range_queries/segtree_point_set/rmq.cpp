18a5224dcb74b5d535abd152958af96af3db1aa6
tomasantonelli <contact@tomas-antonelli.xyz>
2024-06-19 16:33:29 -0300

    Simplifica segment tree RMQ

--- range_queries/segtree_point_set/rmq.cpp
+++ range_queries/segtree_point_set/rmq.cpp
@@ -1,8 +1,26 @@
-struct STNode { int val; };
-STNode operator * (STNode a, STNode b) { return (a.val < b.val) ? a : b; }
-const STNode ST_ID = { INT_MAX };
+#pragma region // Segtree max query
+struct SegtreeRMQ {
+    vector<int> tree;
+    int get (int i) { return tree[i + sz(tree) / 2]; }
+    void update (int i, int x) {
+        for (tree[i += sz(tree) / 2] = x; i > 1; i /= 2)
+            tree[i / 2] = max(tree[i], tree[i ^ 1]);
+    }
+    int query (int i, int j) {
+        int res = INT_MIN;
+        int l = i + sz(tree) / 2;
+        int r = j + sz(tree) / 2 + 1;
+        for (; l < r; l /= 2, r /= 2) {
+            if (l & 1) res = max(res, tree[l++]);
+            if (r & 1) res = max(res, tree[--r]);
+        } return res;
+    }
+};
 
-vector<int> A = { ... };
-vector<STNode> hojas(A.size());
-transform(all(A), begin(hojas), [](int x) { return (STNode){x}; });
-STree T = segtree_build(hojas);
+auto SegtreeRMQ_crear (vector<int>& A) {
+    vector<int> tree(2 * sz(A), INT_MIN);
+    forn(i, sz(A)) tree[i + sz(A)] = A[i];
+    for (int i = sz(A) - 1; i; i--) tree[i] = max(tree[2*i], tree[2*i + 1]);
+    return SegtreeRMQ { tree };
+}
+#pragma endregion
